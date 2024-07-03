7f9f4b08807ab09f62a9cc9e84861a76cc5c28c9
ohjungho <dhwjdgh1122@naver.com>
2024-07-03 13:45:53 +0900

    component dom and events

--- client/main.js
+++ client/main.js
@@ -1,12 +1,52 @@
 
 
 
-const app = document.querySelector('#app');
-const temp = document.querySelector('#temp');
 
 
-const clone = temp.content.cloneNode(true);
+class UserCard extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode:'open'});
+    this.shadowRoot.innerHTML = `
+      <button type="button">btn</button>
+    `
+    this.button = this.shadowRoot.querySelector('button');
+  }
 
-app.appendChild(temp);
+  connectedCallback(){
 
-console.log(temp.content);
+    this.button.addEventListener('click',()=> this.clickMe() )
+
+  }
+
+  
+
+  clickMe(){
+    console.log(this);
+  }
+
+
+
+}
+
+
+customElements.define('user-card',UserCard);
+
+
+
+// console.log( document.querySelector('user-card').shadowRoot.querySelector('button') );
+
+
+
+
+// function sum(){
+//   console.log(this);
+// }
+
+
+// sum() // undefined
+// sum.call({}) // {} => 실행
+// sum.apply({}) // {} => 실행
+// const n = sum.bind({}) // {} => 실행 x 
+
+// n()
