13da429f1434f6701d920a9f9d879a81c74c107f
ohjungho <dhwjdgh1122@naver.com>
2024-07-03 11:36:44 +0900

    templete

--- client/main.js
+++ client/main.js
@@ -1,38 +1,12 @@
-class Button extends HTMLElement {
-
-  constructor(){
-    super();
-
-    // c-button의 쉐도우 돔을 열어줘 동시에 접근할수있는 권한이 부여됨
-    this.attachShadow({mode:'open'});
-
-    console.log(this.shadowRoot);
-
-     // 그리고 그 안에 내가 원하는 태그 집어 넣을거야
-     this.shadowRoot.innerHTML = `
-        <button>hello</button>
-     `
 
 
 
+const app = document.querySelector('#app');
+const temp = document.querySelector('#temp');
 
 
+const clone = temp.content.cloneNode(true);
 
-  }
+app.appendChild(temp);
 
-  connectedCallback(){
-
-  }
-
-  disconnectedCallback(){
-
-  }
-
-
-}
-
-
-customElements.define('c-button',Button);
-
-console.log(document.querySelector('c-button').shadowRoot.
-querySelector('button'));
+console.log(temp.content);
