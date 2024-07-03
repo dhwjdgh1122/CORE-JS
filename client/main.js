13159d277916ff2a74b1fe0ff1f9f575c5863a99
ohjungho <dhwjdgh1122@naver.com>
2024-07-03 10:28:20 +0900

    add

--- client/main.js
+++ client/main.js
@@ -1,3 +1,45 @@
-const a = 'hello';
+class Button extends HTMLElement {
 
-console.log('안녕 웹브라우저!');
+  constructor(){
+    super();
+
+    this.button = document.querySelector('button');
+
+  }
+
+  connectedCallback(){
+    this._render();
+  }
+
+  disconnectedCallback(){
+
+  }
+
+  static get observedAttributes(){
+    return ['id'];
+  }
+
+  attributeChangedCallback(name,oldValue,newValue){
+    if(oldValue !== newValue){
+      this._render()
+    }
+  }
+
+  _render(){
+    this.button.textContent = this.id;
+  }
+
+}
+
+
+customElements.define('c-button',Button);
+
+
+
+const c = document.querySelector('c-button');
+
+let count = 0;
+
+c.addEventListener('click',()=>{
+  c.setAttribute('id',++count)
+})
